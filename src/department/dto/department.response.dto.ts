26dd33ec23086f861bcd02729c6920534fca24ec
Tam Chau <thanhtamag@gmail.com>
2026-01-05 16:21:34 +0700

    Refactor Department API

--- src/department/dto/department.response.dto.ts
+++ src/department/dto/department.response.dto.ts
@@ -1,55 +1,20 @@
-import { TeamShortDto, UserShortDto } from '@/common/dto';
+import { DepartmentShortDto, TeamShortDto, UserShortDto } from '@/common/dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { ValidateNested } from 'class-validator';
 
-export class DepartmentResponseDto {
-  @ApiProperty({ example: '1' })
-  id: number;
-
-  @ApiProperty({ example: 'GDC' })
-  code: string;
-
-  @ApiProperty({ example: 'Global Development Center' })
-  name: string;
-
-  @ApiProperty({ example: 'Global Development Center department' })
-  description?: string;
-
-  @ApiProperty({ type: UserShortDto, nullable: true })
+export class DepartmentResponseDto extends DepartmentShortDto {
+  @Type(() => UserShortDto)
+  @ApiProperty({
+    nullable: true,
+    type: () => UserShortDto,
+  })
   head?: UserShortDto | null;
 
   @ApiProperty({ type: () => [TeamShortDto] })
+  @ValidateNested({ each: true })
+  @Type(() => TeamShortDto)
   teams: TeamShortDto[];
-
-  @ApiProperty({
-    example: '2025-01-01T10:00:00.000Z',
-    type: String,
-    format: 'date-time',
-  })
-  createdAt: Date;
-
-  @ApiProperty({
-    example: '2025-01-02T15:30:00.000Z',
-    type: String,
-    format: 'date-time',
-  })
-  updatedAt: Date;
-}
-
-export class DepartmentShortResponseDto {
-  @ApiProperty({ example: '1' })
-  id: number;
-
-  @ApiProperty({ example: 'GDC' })
-  code: string;
-
-  @ApiProperty({ example: 'Global Development Center' })
-  name: string;
-
-  @ApiProperty({ example: 'Global Development Center department' })
-  description?: string;
-
-  @ApiProperty({ type: UserShortDto, nullable: true })
-  head?: UserShortDto | null;
 
   @ApiProperty({
     example: '2025-01-01T10:00:00.000Z',
