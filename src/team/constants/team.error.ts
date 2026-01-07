f12f8486cd00a335608c066d73e612384ab0b8e2
Tâm Châu <66420913+TamChau@users.noreply.github.com>
2026-01-07 11:21:08 +0700

    Enhance: Team Api (#13)

--- src/team/constants/team.error.ts
+++ src/team/constants/team.error.ts
@@ -1,9 +1,12 @@
-export const teamError = {
+export const TEAM_ERROR_MESSAGE = {
   departmentNotFound: 'Department not found',
   codeExisted: 'Team code already exists',
   notFound: 'Team not found',
   leaderNotFound: 'Team leader not found',
   userNotFound: 'Users not found:',
   cannotDelete: 'Cannot delete. There are still have users in team',
-  deleteSuccess: 'Team deleted successfully',
+  deleteSuccess: 'Team delete successfully',
+  leaderCannotBeMember:
+    'Leader cannot be both leader and member in the same team.',
+  memberExists: 'There are still have member(s) in team',
 } as const;
